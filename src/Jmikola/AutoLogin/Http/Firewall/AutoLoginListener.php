jmikola/AutoLogin

commit 71f0f6232b356e5d8d5943d9ad4369bc26e2b6b9

_GET -> _REQUEST
I do not see why it should only accept token from
query but not from whole request. I want to
pass the token in POST data.

src/Jmikola/AutoLogin/Http/Firewall/AutoLoginListener.php
@@ -53,12 +53,12 @@ class AutoLoginListener implements ListenerInterface
 
         $request = $event->getRequest();
 
-        if (!$request->query->has($this->tokenParam)) {
+        if (!$request->has($this->tokenParam)) {
             return;
         }
 
         try {
-            $token = new AutoLoginToken($this->providerKey, $request->query->get($this->tokenParam));
+            $token = new AutoLoginToken($this->providerKey, $request->get($this->tokenParam));
 
             /* TODO: This authentication method should be considered the same as
              * remember-me according to AuthenticationTrustResolver. That will

commit 0bf6a24c3c4bb6cf1118fdff726d32585bee98fb

Don't call non-existent Request::has() method in AutoLoginListener
This fixes a bug introduced in 71f0f6232b356e5d8d5943d9ad4369bc26e2b6b9.

src/Jmikola/AutoLogin/Http/Firewall/AutoLoginListener.php
@@ -4,6 +4,7 @@ namespace Jmikola\AutoLogin\Http\Firewall;
 
 use Jmikola\AutoLogin\Authentication\Token\AutoLoginToken;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Log\LoggerInterface;
 use Symfony\Component\Security\Core\SecurityContextInterface;
@@ -53,7 +54,7 @@ class AutoLoginListener implements ListenerInterface
 
         $request = $event->getRequest();
 
-        if (!$request->has($this->tokenParam)) {
+        if ( ! ($this->isTokenInRequest($request))) {
             return;
         }
 
@@ -65,7 +66,7 @@ class AutoLoginListener implements ListenerInterface
              * entail either refactoring to extend RememberMeToken or adding a
              * service to compose the existing AuthenticationTrustResolver and
              * implement custom logic to respect our own token class.
-             */ 
+             */
             if ($authenticatedToken = $this->authenticationManager->authenticate($token)) {
                 $this->securityContext->setToken($authenticatedToken);
 
@@ -83,5 +84,18 @@ class AutoLoginListener implements ListenerInterface
                 );
             }
         }
+    }
+
+    /**
+     * Check the ParameterBags consulted by Request::get() for the token.
+     *
+     * @param Request $request
+     * @return boolean
+     */
+    private function isTokenInRequest(Request $request)
+    {
+        return $request->query->has($this->tokenParam) ||
+            $request->attributes->has($this->tokenParam) ||
+            $request->request->has($this->tokenParam);
     }
 }