93fd9bbdfe0427a9626f390cb5f5b50f931d8736
Rasmus Nilsson <rasmus.nilsson@lokalguiden.se>
2021-05-25 21:44:50 +0200

    Enable autoloading for Tests folder

--- Tests/Http/Firewall/AutoLoginListenerTest.php
+++ Tests/Http/Firewall/AutoLoginListenerTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Tests\AutoLogin\Http\Firewall;
+namespace Jmikola\AutoLogin\Tests\Http\Firewall;
 
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
