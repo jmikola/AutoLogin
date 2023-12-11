2986c833dc9599ce207f73c85e788c2da0fd2f3f
Peter Dietrich <xosofox@gmail.com>
2023-12-11 12:38:02 +0100

    trigger deprecation

--- src/User/AutoLoginUserProviderInterface.php
+++ src/User/AutoLoginUserProviderInterface.php
@@ -3,7 +3,11 @@
 namespace Jmikola\AutoLogin\User;
 
 use Jmikola\AutoLogin\Exception\AutoLoginTokenNotFoundException;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+
+@trigger_deprecation('jmikola/autologin', '6.0', 'The "%s" class is deprecated, use Symfony Components with AccessTokenAuthenticator instead. (see README.md)', AutoLoginUserProviderInterface::class);
 
 interface AutoLoginUserProviderInterface
 {
